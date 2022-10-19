3b06bae59eb640e1b263113559e5491286e0091a
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-10-19 16:33:41 -0700

    moved menu into a component

--- pass-bakery-fe/src/App.js
+++ pass-bakery-fe/src/App.js
@@ -1,4 +1,5 @@
 import TitleNav from './components/TitleNav';
+import Menu from './components/Menu';
 import './App.css';
 
 function App() {
@@ -6,95 +7,7 @@ function App() {
     <body>
         <TitleNav></TitleNav>
         <hr/>
-        <div id="items-container">
-            <h2 id="menu-title">Coffee</h2>
-            <ul>
-                <li>
-                    <button name="espresso">
-                        <span id="button-title">Espresso</span><br/>
-                        <span id="button-price">$2.75</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="cappuccino">
-                        <span id="button-title">Cappuccino</span><br/>
-                        <span id="button-price">$3.50</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="latte">
-                        <span id="button-title">Latte</span><br/>
-                        <span id="button-price">$4.00</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="drip">
-                        <span id="button-title">Drip</span><br/>
-                        <span id="button-price">$2.50</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="pour-over">
-                        <span id="button-title">Pour Over</span><br/>
-                        <span id="button-price">$4.75</span>
-                    </button>
-                </li>
-            </ul>
-            <h2 id="menu-title">Breakfast</h2>
-            <ul>
-                <li>
-                    <button name="oatmeal">
-                        <span id="button-title">Oatmeal</span><br/>
-                        <span id="button-price">$4.00</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="toast">
-                        <span id="button-title">Toast</span><br/>
-                        <span id="button-price">$3.50</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="egg-sandwich">
-                        <span id="button-title">Egg Sandwich</span><br/>
-                        <span id="button-price">$5.50</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="egg-cheese-sandwich">
-                        <span id="button-title">Egg and Cheese Sandwich</span><br/>
-                        <span id="button-price">$6.50</span>
-                    </button>
-                </li>
-            </ul>
-            <h2 id="menu-title">Breads</h2>
-            <ul>
-                <li>
-                    <button name="pain-campagne">
-                        <span id="button-title">Pain de campagne</span><br/>
-                        <span id="button-price">$7.00</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="olive-lemon">
-                        <span id="button-title">Olive and lemon</span><br/>
-                        <span id="button-price">$7.50</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="brioche">
-                        <span id="button-title">Brioche</span><br/>
-                        <span id="button-price">$8.00</span>
-                    </button>
-                </li>
-                <li>
-                    <button name="ciabatta">
-                        <span id="button-title">Ciabatta</span><br/>
-                        <span id="button-price">$5.50</span>
-                    </button>
-                </li>
-            </ul>
-        </div>
+        <Menu></Menu>
     </body>
   );
 }
