95a467b367b8e22c7a0f8eb259f509d23b866b3f
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-10-24 16:44:29 -0700

    forgot to add props

--- pass-bakery-fe/src/components/BreadList.jsx
+++ pass-bakery-fe/src/components/BreadList.jsx
@@ -1,6 +1,6 @@
 import BuildCategoryList from './BuildCategoryList.jsx';
 
-function BreadList() {
+function BreadList(props) {
     return (
         <>
             <BuildCategoryList name={props.secData.name} offerings={props.secData.offerings} />
--- pass-bakery-fe/src/components/BreakfastList.jsx
+++ pass-bakery-fe/src/components/BreakfastList.jsx
@@ -1,6 +1,6 @@
 import BuildCategoryList from './BuildCategoryList.jsx';
 
-function BreakfastList() {
+function BreakfastList(props) {
     return (
         <>
             <BuildCategoryList name={props.secData.name} offerings={props.secData.offerings} />
