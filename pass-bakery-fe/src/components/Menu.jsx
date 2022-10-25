c88b131be7a4becf9a7c7a42c3a13a342ae25276
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-10-25 13:02:36 -0700

    call by key (name) instead of index

--- pass-bakery-fe/src/components/Menu.jsx
+++ pass-bakery-fe/src/components/Menu.jsx
@@ -3,12 +3,11 @@ import Breakfast from './BreakfastList';
 import Breads from './BreadList';
 
 function Menu(props) {
-    // coffee is 0, breakfast is 1, breads are 2
     return (
         <div id="items-container">
-            <Coffee secData={props.data[0]}></Coffee>
-            <Breakfast secData={props.data[1]}></Breakfast>
-            <Breads secData={props.data[2]}></Breads>
+            <Coffee secData={props.data.Coffee}></Coffee>
+            <Breakfast secData={props.data.Breakfast}></Breakfast>
+            <Breads secData={props.data.Bread}></Breads>
         </div>
     );
 }
