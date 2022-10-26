fd8f55d738a3c6ad19982474d9eadc827230bb94
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-10-26 08:26:07 -0700

    styled (the) components

--- pass-bakery-fe/src/components/BuildCategoryList.jsx
+++ pass-bakery-fe/src/components/BuildCategoryList.jsx
@@ -1,21 +1,62 @@
+import styled from "styled-components";
+
+const Title = styled.h2`
+  font-weight: bold;
+  font-size: 24px;
+  margin-left: 32px;
+  margin-bottom: 4px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 32px;
+  list-style-type: none;
+`;
+
+const ListItem = styled.li`
+  width: 48%;
+  margin: 8px;
+`;
+
+const ItemButton = styled.button`
+  width: 100%;
+  background-color: white;
+  border: 2px solid lightgray;
+  border-radius: 4px;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const ButtonTitle = styled.span`
+  font-weight: bold;
+`;
+
+const ButtonPrice = styled.span`
+  font-style: italic;
+  color: gray;
+`;
+
 function BuildCategoryList(section) {
   const name = section.name;
   const offerings = section.offerings;
 
   return (
     <>
-      <h2 className="menu-title">{name}</h2>
-      <ul>
+      <Title>{name}</Title>
+      <List>
         {offerings.map((item, idx) => (
-          <li key={idx}>
-            <button name={`${item.name}`}>
-              <span className="button-title">{item.name}</span>
+          <ListItem key={idx}>
+            <ItemButton>
+              <ButtonTitle>{item.name}</ButtonTitle>
               <br />
-              <span className="button-price">${item.price}</span>
-            </button>
-          </li>
+              <ButtonPrice>${item.price}</ButtonPrice>
+            </ItemButton>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
--- pass-bakery-fe/src/components/TitleNav.jsx
+++ pass-bakery-fe/src/components/TitleNav.jsx
@@ -1,23 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 import logo from "../images/1p-site-logo.svg";
 import bread from "../images/bread.jpeg";
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding: 16px;
+`;
+
+const Title = styled.h1`
+  margin: auto;
+  font-family: "Qwitcher Grypen";
+  font-size: 128px;
+`;
+
+const Hours = styled.h2`
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 4px;
+`;
+
+const BreadImage = styled.img`
+  max-width: 512px;
+  height: auto;
+`;
+
+const Info = styled.p`
+  margin-bottom: 32px;
+`;
+
 function TitleNav() {
   return (
-    <div id="head-container">
+    <TitleContainer>
       <div id="title-hours">
         <img src={logo} alt="one pass logo" />
-        <h1>Pass Bakery</h1>
-        <p>620 N 25th St, San Francisco, CA</p>
-        <h2>Bakery Hours:</h2>
-        <p>
+        <Title>Pass Bakery</Title>
+        <Info>620 N 25th St, San Francisco, CA</Info>
+        <Hours>Bakery Hours:</Hours>
+        <Info>
           Mon-Fri 6AM-5PM PT,
           <br />
           Sat-Sun 7AM-4PM PT
-        </p>
+        </Info>
       </div>
-      <img id="bread-image" src={bread} alt="fresh baked bread" />
-    </div>
+      <BreadImage src={bread} alt="fresh baked bread" />
+    </TitleContainer>
   );
 }
 
