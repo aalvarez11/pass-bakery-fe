ce825aa88a889f1d0a83490083e5095114c66d90
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-10-27 11:53:31 -0700

    create component that holds bakery info and menu

---
+++ pass-bakery-fe/src/components/BakeryMain.jsx
@@ -0,0 +1,26 @@
+import { menuData } from "../data/menuData";
+import TitleNav from "./TitleNav";
+import Menu from "./Menu";
+import styled from "styled-components";
+
+const StyledContainer = styled.div``;
+
+const StyledHorizontalRule = styled.hr`
+  border-color: lightgray;
+`;
+
+function BakeryMain(props) {
+  const menuObj = {};
+  menuData.menu.forEach((item, idx) => {
+    menuObj[menuData.menu[idx].name] = item;
+  });
+
+  return (
+    <StyledContainer className={props.className}>
+      <TitleNav></TitleNav>
+      <Menu data={menuObj}></Menu>
+    </StyledContainer>
+  );
+}
+
+export default BakeryMain;
