b4d91e12cbe674cacea5f8d3f3a6b357788bf306
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-10-27 16:12:28 -0700

    resized logo, convert to class, hideable message

--- pass-bakery-fe/src/components/TitleNav.jsx
+++ pass-bakery-fe/src/components/TitleNav.jsx
@@ -6,13 +6,20 @@ import bread from "../images/bread.jpeg";
 const StyledTitleContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
-  padding: 16px;
+  padding-top: 16px;
+  padding-bottom: 16px;
   border-bottom: 1px solid lightgray;
+`;
+
+const StyledLogo = styled.img`
+  max-height: 16px;
+  width: auto;
 `;
 
 const StyledTitle = styled.h1`
   margin: auto;
   font-family: "Qwitcher Grypen";
+  font-weight: normal;
   font-size: 128px;
 `;
 
@@ -31,23 +38,49 @@ const StyledInfo = styled.p`
   margin-bottom: 32px;
 `;
 
-function TitleNav() {
-  return (
-    <StyledTitleContainer>
-      <div id="title-hours">
-        <img src={logo} alt="one pass logo" />
-        <StyledTitle>Pass Bakery</StyledTitle>
-        <StyledInfo>620 N 25th St, San Francisco, CA</StyledInfo>
-        <StyledHours>Bakery Hours:</StyledHours>
-        <StyledInfo>
-          Mon-Fri 6AM-5PM PT,
-          <br />
-          Sat-Sun 7AM-4PM PT
-        </StyledInfo>
-      </div>
-      <StyledBreadImage src={bread} alt="fresh baked bread" />
-    </StyledTitleContainer>
-  );
+class TitleNav extends React.Component {
+  constructor() {
+    super();
+    var today = new Date();
+    var hour = today.getHours();
+    var day = today.getDay();
+
+    this.state = {
+      currHour: hour,
+      currDay: day,
+    };
+  }
+
+  render() {
+    let closedMessage = "";
+
+    if (
+      ((this.state.currDay == 0 || this.state.currDay == 6) &&
+        (this.state.currHour < 7 || this.state.currHour > 15)) ||
+      (this.state.currDay > 0 &&
+        this.state.currDay < 6 &&
+        (this.state.currHour < 6 || this.state.currHour > 16))
+    ) {
+      closedMessage = "BAKERY IS CLOSED.";
+    }
+
+    return (
+      <StyledTitleContainer>
+        <div id="title-hours">
+          <StyledLogo src={logo} alt="the One Pass logo" />
+          <StyledTitle>Pass Bakery</StyledTitle>
+          <StyledHours>Hours</StyledHours>
+          <StyledInfo>
+            Mon-Fri 6AM-5PM PT,
+            <br />
+            Sat-Sun 7AM-4PM PT
+          </StyledInfo>
+          <StyledInfo className="bakery-closed">{closedMessage}</StyledInfo>
+        </div>
+        <StyledBreadImage src={bread} alt="fresh baked bread" />
+      </StyledTitleContainer>
+    );
+  }
 }
 
 export default TitleNav;
