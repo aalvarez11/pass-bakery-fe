ec89970add97c3be9cd1c45e3bd10648df073236
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-11-01 14:29:31 -0700

    create slice for the store

---
+++ pass-bakery-fe/src/redux/cartSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  itemQuantity: 0,
+  total: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const itemExists = state.cartItems.find(
+        (item) => item.name === action.payload.name
+      );
+      if (itemExists === undefined) {
+        state.cartItems.push(action.payload);
+      }
+      cartQuantity++;
+      cartTotal += parseFloat(action.payload.price);
+    },
+    removeItem(state, action) {
+      const idx = state.cartItems.indexOf(action.payload.name);
+      state.cartItems.splice(idx, 1);
+      cartQuantity--;
+      cartTotal -= parseFloat(action.payload.price);
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
