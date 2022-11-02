b20fa1ca1ba209c8d29d7bb61b677e46bf28ee5f
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-11-02 16:32:49 -0700

    fix store export

--- pass-bakery-fe/src/redux/store.js
+++ pass-bakery-fe/src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
 
-const Store = configureStore({
+export const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
 });
-
-export default Store;
