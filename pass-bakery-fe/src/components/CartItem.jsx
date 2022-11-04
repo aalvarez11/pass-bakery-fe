0a2c8aa55a02b5b51214dc89e62dc294966cca20
aalvarez11 <alejandro.alvarez@rallyhealth.com>
2022-11-04 11:21:01 -0700

    cleaned up styling, dispatch for removing

--- pass-bakery-fe/src/components/CartItem.jsx
+++ pass-bakery-fe/src/components/CartItem.jsx
@@ -3,18 +3,22 @@ import { removeItem } from "../redux/cartSlice";
 import styled from "styled-components";
 import { formatCurrency } from "../helpers/formatCurrency";
 
-const StyledItemContainer = styled.div`
+const StyledItemContainer = styled.p`
   display: flex;
   justify-content: space-between;
-  p.cart__item {
-    font-weight: bold;
-  }
+  font-weight: bold;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-bottom: 8px;
 `;
 
 const StyledRemoveButton = styled.p`
   font-size: 0.8em;
   color: darkolivegreen;
-  background-color: #c5caba;
+  margin-left: 24px;
+  span {
+    background-color: #c5caba;
+  }
 `;
 
 function CartItem(item) {
@@ -23,14 +27,16 @@ function CartItem(item) {
 
   return (
     <>
-      <StyledItemContainer>
-        <p className="cart__item">
-          <span>{item.name}</span>
-          <span>{itemPrice}</span>
-        </p>
+      <StyledItemContainer className="cart__item">
+        <span>{item.name}</span>
+        <span>{itemPrice}</span>
       </StyledItemContainer>
-      <StyledRemoveButton onClick={dispatch(removeItem(item.name))}>
-        remove
+      <StyledRemoveButton
+        onClick={() => {
+          dispatch(removeItem(item.name));
+        }}
+      >
+        <span>remove</span>
       </StyledRemoveButton>
     </>
   );
